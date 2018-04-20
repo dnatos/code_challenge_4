a49252155da7f86f318377d3bf0b2754a1ac0a2d
Dimitrios Natos <dimnat@gounaras.home>
2018-04-20 09:43:48 -0400

    Try-Catch added

--- santa.js
+++ santa.js
@@ -3,19 +3,20 @@ const fs = require('fs');
 let floor = 0;
 let basement = 0;
 
+try {
+  const content = fs.readFileSync('./input.txt', 'utf8');
+  console.time('code_challenge_4');
 
-const content = fs.readFileSync('./input.txt', 'utf8');
+  for (let i = 0; i < content.length; i++) {
+    content[i] === '(' ? floor++ : floor--;
 
-console.time('code_challenge_4');
-
-for (let i = 0; i < content.length; i++) {
-	content[i] === '(' ? floor++ : floor--;
-
-	if(floor === -1 && basement < 1) {
-		console.log('basement position:', i+1);
-		basement++;
-	}
+    if (floor === -1 && basement < 1) {
+      console.log('basement position:', i+1);
+      basement++;
+    }
+  }
+} catch (e) {
+  console.log('Something wrong happend!', e);
 }
-
-console.log("floor:", floor);
+console.log('floor:', floor);
 console.timeEnd('code_challenge_4');
